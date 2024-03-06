2372c0f0150aa5a603a9d9fd303e855c22ad253b
tatianaTun <tatianatun92@gmail.com>
2024-03-05 20:20:45 -0800

    Minor changes and added consts.js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header/Header";
+import HomePage from "./pages/HomePage/HomePage";
+import VideoDetailsPage from "./pages/VideoDetailsPage/VideoDetailsPage";
+import UploadPage from "./pages/UploadPage/UploadPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/:videoId" element={<VideoDetailsPage />}></Route>
+        <Route path="/upload" element={<UploadPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/consts.js
@@ -0,0 +1,4 @@
+const baseURL = 'https://unit-3-project-api-0a5620414506.herokuapp.com/'
+const apiKey = 'd7e4b077-116e-4d59-b14a-83f4643cb825'
+
+export { baseURL,apiKey }
